ee6eea87f747cced705be10e8e58ba3fb7684e6d
DESKTOP-68PR451\kiril <akimow.kirill@yandex.ru>
2021-03-22 02:10:23 +0300

    initcommit

---
+++ src/Stack.java
@@ -0,0 +1,35 @@
+public class Stack {
+    public String sumword(String word, int number){
+        String result ="";
+        for (int i = 0; i < number; i++) {
+            result = result + word;
+        }
+        return result;
+    }
+    public String alg (String sourceline) {
+        String result = "";
+        int i = 0;
+        int liopen = 0;
+        int liclose = 0;
+        String word = "";
+        liopen = sourceline.lastIndexOf('[');
+        liclose = sourceline.indexOf(']', liopen);
+        word = sourceline.substring(liopen + 1, liclose);
+        //System.out.println(word);
+        int number = Integer.parseInt(String.valueOf(sourceline.charAt(liopen - 1)));
+        //System.out.println(number);
+        sourceline = sourceline.substring(0, liopen - 1) + sumword(word, number) + sourceline.substring(liclose+1);
+        if (sourceline.indexOf('[') != -1) {
+            sourceline = alg(sourceline);
+        }
+        return sourceline;
+    }
+
+    public static void main(String[] args) {
+        Stack blabla = new Stack();
+        System.out.println(blabla.alg("2[3[x]y]"));
+        //System.out.println(blabla.superalgoritm("3[xy]2[qwe]"));
+        //System.out.println(blabla.superalgoritm("2[2[3[x]y]4[z]]"));
+        System.out.println();
+    }
+}
